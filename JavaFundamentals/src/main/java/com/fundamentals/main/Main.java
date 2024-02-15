7849819bfdfc09d7eac65810b9e248bb0c310583
VpProctor <vproctor23@gmail.com>
2024-02-15 16:17:36 -0600

    Lesson 3- My application overview

--- JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
+++ JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
@@ -3,5 +3,22 @@ package com.fundamentals.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        houseExample();
+    }
+
+
+    // Single Comment
+    /*Multi-line comment
+    This is a second line
+    And a third.
+    * */
+
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.doorFunction();
+        myHouse.entranceDoorColor = "Red";
+        myHouse.roofStyle = "Cedar Shingle";
+        myHouse.totalWindows = 30;
+        System.out.println(myHouse.entranceDoorColor);
     }
 }
