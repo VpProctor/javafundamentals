f1fb4152cfb0febe2d53c20f5ec4c4222201d376
VpProctor <vproctor23@gmail.com>
2024-03-12 09:35:09 -0500

    Lab1

---
+++ JavaFundamentals/src/main/java/com/fundamentals/labs/Lab1.java
@@ -0,0 +1,15 @@
+package com.fundamentals.labs;
+
+import java.sql.SQLOutput;
+
+public class Lab1 {
+    public static void main(String[] argos) {
+        basicStatements();
+    }
+    public static void basicStatements() {
+        System.out.println("This is my first sentence");
+        System.out.println(" I am new to Java");
+        System.out.println("This Java course is cool");
+        System.out.println("I am learning stuff every day");
+    }
+}
