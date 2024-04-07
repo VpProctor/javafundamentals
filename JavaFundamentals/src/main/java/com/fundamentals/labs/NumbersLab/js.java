c1727b91b040db0007e373fda5a2a2bc032acc3c
VpProctor <vproctor23@gmail.com>
2024-04-07 15:26:54 -0500

    Primitive Lab
    Primitive Lab

---
+++ JavaFundamentals/src/main/java/com/fundamentals/labs/NumbersLab/js.java
@@ -0,0 +1,33 @@
+package com.fundamentals.labs.NumbersLab;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.function.DoubleBinaryOperator;
+
+public class js {
+
+    public static void main (String[] args){
+        int decimalNumber = 29;
+        String binaryCode = Integer.toBinaryString(29);
+
+        System.out.println("The binaryCode for 29 is:" + binaryCode);
+        
+
+    }
+    // Conversion for Widening from byte to int
+    public static void main(String[] args) {
+        byte byteValue =50;
+        short shortValue= 500;
+        int intValue = 5,000;
+        
+        Conversion(byteValue,shortValue, intValue);
+        //Widening conversion byte to int
+        int conversionByte = byteParam;
+        System.out.println("Conversion short to long;" + conversionShort);
+
+
+
+
+}
+
+}
