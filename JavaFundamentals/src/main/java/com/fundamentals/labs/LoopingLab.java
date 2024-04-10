c04812f64730ad2683b114f1629e8867f3b5c3c3
VpProctor <vproctor23@gmail.com>
2024-04-10 09:26:43 -0500

    Looping Lab

--- JavaFundamentals/src/main/java/com/fundamentals/labs/LoopingLab.java
+++ JavaFundamentals/src/main/java/com/fundamentals/labs/LoopingLab.java
@@ -1,2 +1,33 @@
-package com.fundamentals.labs;public interface LoopingLab {
-}
+package com.fundamentals.labs;
+
+import java.sql.SQLOutput;
+
+public class LoopingLab {
+    public static void main(String[] args) {
+        Numberdisplay();
+    }
+    public static void Numberdisplay() {
+        int count =0;
+        while (count <= 15) {
+            if(count == 5) {
+                System.out.println("five");
+            } else if (count == 10) {
+                System.out.println("ten");
+            } else if (count == 15){
+                System.out.println("fifteen");
+                } else {
+                System.out.println(count);
+            }
+            count++;
+        }
+    }// End Method
+
+        public static void DivideByThree() {
+        for (int i = 0; i <= 30; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}// End Class
